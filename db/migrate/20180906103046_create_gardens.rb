dd687e1ae2d2473838dc9ad242a240af432533c9
Rémi Boivin <remi.boivin@epitech.eu>
2018-09-16 22:59:26 +0200

    Modif Garden table association

--- db/migrate/20180906103046_create_gardens.rb
+++ db/migrate/20180906103046_create_gardens.rb
@@ -2,9 +2,9 @@ class CreateGardens < ActiveRecord::Migration[5.2]
 
   def change
     create_table :gardens do |t|
-    	t.belongs_to :plantae
+      t.belongs_to :plantae
+      t.belongs_to :sol
     	t.integer :plantae_id2
-
     	t.integer :source
 
 		t.timestamps
